5015c15c51c24ae2226c30ab7f73d2d415edb8d8
Tyler Smith <tylertsmith14@gmail.com>
2014-01-09 02:32:11 -0500

    Unicorn Config Tweak

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,7 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "#{root}/tmp/unicorn.tylersmith.sock"
+listen "/tmp/unicorn.tylersmith.sock"
 worker_processes 2
 timeout 30
 
